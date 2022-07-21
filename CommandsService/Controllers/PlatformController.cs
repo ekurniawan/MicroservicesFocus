3ff3a729be1234407b65d052518e1e08ee27ce7d
Erick Kurniawan <erick.kurniawan@gmail.com>
2022-07-21 13:19:34 +0700

    menambahkan RabbitMQ Console

--- CommandsService/Controllers/PlatformController.cs
+++ CommandsService/Controllers/PlatformController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -10,16 +13,31 @@ namespace CommandsService.Controllers
     [Route("api/c/[controller]")]
     public class PlatformController : ControllerBase
     {
-        public PlatformController()
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformController(ICommandRepo repository, IMapper mapper)
         {
-            
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms from CommandsService");
+
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItems));
         }
 
         [HttpPost]
         public ActionResult TestInboundConnection()
         {
-            Console.WriteLine("--> Inbound POST Command Service");
-            return Ok("Inbound test from Platforms Controller - Command Service");
+            Console.WriteLine("--> Inbound POST # Command Service");
+
+            return Ok("Inbound test of from Platforms Controler");
         }
     }
 }
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue",
+        durable:true,
+        exclusive:false,
+        autoDelete:false,
+        arguments:null);
+
+            var count = 0;
+            while(true)
+            {
+                var message = new { Name = "Producer", Message = $"Hello Count: {count}" };
+                var body = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(message));
+                channel.BasicPublish("", "demo-queue", null, body);
+                count++;
+                Thread.Sleep(1000);
+            }
